e77cb98cea71347c9f48b1149b73d1c8dbd9dcb3
Isaac916 <tojito916@gmail.com>
2023-10-19 13:00:00 +0200

    Cambios en la pantalla de incidencias en .net

--- 2damTransita3Interfaces/Pantalla_Cliente/Incidencias.cs
+++ 2damTransita3Interfaces/Pantalla_Cliente/Incidencias.cs
@@ -7,7 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 namespace Pantalla_Cliente
 {
     public partial class Incidencias : Form
@@ -15,17 +18,91 @@ namespace Pantalla_Cliente
         public Incidencias()
         {
             InitializeComponent();
+            btn_filtrar.FlatStyle = FlatStyle.Flat;
+            btn_filtrar.FlatAppearance.BorderSize = 0;
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.ControlBox = false;
+            buscarTextBox.LostFocus += new EventHandler(buscarTextBox_LostFocus);
+            this.Click += new EventHandler(incidencias_Click);
+            buscarTextBox.Click += new EventHandler(miTextBox_Click);
+            buscarTextBox.Leave += new EventHandler(miTextBox_Leave);
+            // Establece el alineamiento horizontal del texto en el centro
+            buscarTextBox.Multiline = true; // Asegura que el TextBox sea de varias líneas
+            buscarTextBox.TextAlign = HorizontalAlignment.Left; // Alinea el texto a la izquierda
+            // Ajusta el espaciado superior para centrar verticalmente el texto
+            buscarTextBox.Padding = new Padding(20, 20, 0, 0);
+
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None; // Opcional, quita el borde de la ventana
+            //this.Icon = Properties.Resources.icono_de_aplicacion; // Cambia el icono de la ventana
+            //this.BackgroundImage = Properties.Resources.fondo_de_la_ventana; // Cambia el fondo de la ventana
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            this.Text = "Título de la Ventana"; // Cambia el título de la ventana
+            //this.BackgroundImage = Properties.Resources.fondo_de_la_ventana; // Cambia el fondo de la ventana
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
 
         }
 
         private void Incidencias_Load(object sender, EventArgs e)
         {
 
+            /*string connectionString = "tu_cadena_de_conexión"; // Reemplaza con la cadena de conexión a tu base de datos
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Consulta SQL para obtener datos de la base de datos
+                string query = "SELECT * FROM TuTabla"; // Reemplaza con tu consulta SQL
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        // Itera a través de los datos y agrégalos al ListView
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            ListViewItem item = new ListViewItem(row["Columna1"].ToString());
+                            item.SubItems.Add(row["Columna2"].ToString());
+                            item.SubItems.Add(row["Columna3"].ToString());
+
+                            //listView1.Items.Add(item);
+                        }
+                    }
+                }
+            }*/
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
 
+        }
+        private void miTextBox_Click(object sender, EventArgs e)
+        {
+            // Cuando se hace clic en el TextBox, borra el texto
+            buscarTextBox.Text = "";
+        }
+        private void buscarTextBox_LostFocus(object sender, EventArgs e)
+        {
+            buscarTextBox.ReadOnly = true; // Establece el TextBox en modo de solo lectura
+            
+        }
+
+        private void incidencias_Click(object sender, EventArgs e)
+        {
+            buscarTextBox.ReadOnly = true; // Establece el TextBox en modo de solo lectura
+        }
+
+        private void miTextBox_Leave(object sender, EventArgs e)
+        {
+            // Cuando el TextBox pierde el foco, restaura el texto
+            if (string.IsNullOrWhiteSpace(buscarTextBox.Text))
+            {
+                buscarTextBox.Text = "Buscar"; // Puedes cambiar este texto predeterminado
+            }
         }
 
         private void lab_Click(object sender, EventArgs e)
@@ -47,5 +124,38 @@ namespace Pantalla_Cliente
             f.Show();
         }
 
+        private void imgTransita_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+      
+
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fondoGrisMid_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
