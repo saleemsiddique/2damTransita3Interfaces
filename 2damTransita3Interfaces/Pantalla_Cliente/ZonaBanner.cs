a705feb320d02be55241a7b451bd17a3eb6b7487
Isaac916 <tojito916@gmail.com>
2023-10-30 17:04:06 +0100

    updates

--- 2damTransita3Interfaces/Pantalla_Cliente/ZonaBanner.cs
+++ 2damTransita3Interfaces/Pantalla_Cliente/ZonaBanner.cs
@@ -16,5 +16,10 @@ namespace Pantalla_Cliente
         {
             InitializeComponent();
         }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
