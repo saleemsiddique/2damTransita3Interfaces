87ec856053a8d7b601295d1808b2fa318912ed5e
Álex Martín <lalexl333games@outlook.es>
2024-01-23 12:31:23 +0100

    Commit 23/01/2024
    Commit con:
       - Limpieza de codigo

--- 2damTransita3Interfaces/Pantalla_Cliente/Conexion.cs
+++ 2damTransita3Interfaces/Pantalla_Cliente/Conexion.cs
@@ -4,28 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pantalla_Cliente
 {
